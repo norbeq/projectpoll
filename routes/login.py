acbf419fdcaf1eb2401149a57487f438056f030d
norbeq <norbertmiazek@gmail.com>
2017-06-02 01:10:07 +0200

    [UPDATE] Authentication JWT Process

--- routes/login.py
+++ routes/login.py
@@ -1,40 +1,37 @@
-from flask import Blueprint, send_from_directory, request, json, Flask
+from flask import Blueprint, send_from_directory, request, json, Flask, current_app as app
 from model.user import User
 from model.authentication import Authentication
 from model.model import db
 import uuid
 import hashlib
+import jwt
+import datetime
+from util.http_response import ForbiddenResponse, JsonResponse, BadRequestResponse
 
-app = Flask(__name__)
 login_api = Blueprint('login_api', __name__)
 
 @login_api.route('/login', methods=['POST'])
 def login():
-
-    body = request.get_json()
+    try:
+        body = request.get_json()
+    except:
+        return BadRequestResponse({"success": False, "message": "Wrong json"})
 
     email = body.get('email')
     digest = body.get('digest')
 
     user = User.query.filter_by(email=email).first()
     if user is None:
-        response = app.response_class(
-            response=json.dumps({"success": False}),
-            status=403,
-            mimetype='application/json'
-        )
-        return response
+        return ForbiddenResponse(obj={"success": False})
 
     last_authentication = Authentication.query.filter_by(user_id=user.id,type="salt",is_valid=True).order_by(Authentication.created.desc()).first()
+    if not last_authentication:
+        return ForbiddenResponse(obj={"success": False})
+
     correct_password = hashlib.sha512((str(user.password)+":"+str(last_authentication.salt)).encode('utf-8')).hexdigest()
 
     if digest != correct_password:
-        response = app.response_class(
-            response=json.dumps({"success": False}),
-            status=403,
-            mimetype='application/json'
-        )
-        return response
+        return ForbiddenResponse(obj={"success": False})
 
     token_uuid = uuid.uuid4()
     last_authentication.type = "token"
@@ -44,30 +41,36 @@ def login():
     Authentication.query.filter_by(user_id=user.id, type="salt").update({"is_valid":False})
     db.session.commit()
 
-    data = {"success": True, "token": token_uuid}
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+    token_payload = {
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'token_uuid': str(token_uuid),
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=app.config['token']['expiration_seconds']),
+        'iat': datetime.datetime.utcnow(),
+        'iss': app.config['token']['iss']
+    }
+    token = jwt.encode(token_payload, app.config['token']['key'], algorithm='HS256')
+    data = {"success": True, "token": str(token)}
+    return JsonResponse(data)
 
 @login_api.route('/pre_login', methods=['POST'])
 def pre_login():
-    body = request.get_json()
+    try:
+        body = request.get_json()
+    except:
+        return BadRequestResponse({"success": False, "message": "Wrong json"})
+
     salt = uuid.uuid4()
     email = body.get('email')
     user = User.query.filter_by(email=email).first()
 
-    if user is not None:
-        authentication = Authentication(user.id, salt, 'salt')
-        db.session.add(authentication)
-        db.session.commit()
-    data = {"success": True, "salt": salt}
+    if user is None:
+        return ForbiddenResponse(obj={"success": False})
 
-    response = app.response_class(
-        response=json.dumps(data),
-        status=200,
-        mimetype='application/json'
-    )
-    return response
+    authentication = Authentication(user.id, salt, 'salt')
+    db.session.add(authentication)
+    db.session.commit()
+
+    data = {"success": True, "salt": salt}
+    return JsonResponse(data)
