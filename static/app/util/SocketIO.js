79d7611fbe64de0f21ecdf6e363659a752c5990d
norbeq <norbertmiazek@gmail.com>
2017-08-25 23:28:11 +0200

    [ADD] SocketIO util

---
+++ static/app/util/SocketIO.js
@@ -0,0 +1,37 @@
+Ext.define('PP.util.SocketIO', {
+    extend: 'Ext.util.Observable',
+
+    constructor: function (config) {
+        var me = this;
+        me.superclass.constructor.call(
+            this
+        );
+
+        me.socket = io.connect(config['host'], {
+            'port': config['port'],
+            'reconnect': config['reconnect'],
+            'reconnection delay': config['reconnection delay'],
+            'max reconnection attempts': config['max reconnection attemps'],
+            'transports': ['websocket', 'flashsocket', 'htmlfile', 'xhr-multipart', 'xhr-polling'],
+            extraHeaders: {
+                auth: PP.util.Security.getToken()
+            }
+        });
+
+        me.socket.on('connect', function () {
+            me.fireEvent('socketconnect', me, this, arguments);
+        });
+
+        me.socket.on('disconnect', function () {
+            me.fireEvent('socketdisconnect', me, this, arguments);
+        });
+
+        me.socket.on('message', function (data) {
+            me.message(data);
+        });
+    },
+
+    message: function (data) {
+        console.log(data);
+    }
+});
