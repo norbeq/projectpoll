f9299463b760fd11c67e96491735e92a6fd8a404
norbeq <norbertmiazek@gmail.com>
2017-08-27 15:15:59 +0200

    [API] Custom Response

--- util/http_response.py
+++ util/http_response.py
@@ -1,11 +1,21 @@
 from flask import json, Flask, current_app as app
 
+
+def AuthenticationFailureResponse():
+    return app.response_class(
+        response=json.dumps({'message': 'Błąd tokenu autoryzacyjnego', 'success': False}),
+        status=403,
+        mimetype='application/json'
+    );
+
+
 def ForbiddenResponse(obj={}):
     return app.response_class(
-            response=json.dumps(obj),
-            status=403,
-            mimetype='application/json'
-        );
+        response=json.dumps(obj),
+        status=403,
+        mimetype='application/json'
+    );
+
 
 def JsonResponse(obj={}):
     return app.response_class(
@@ -14,9 +24,17 @@ def JsonResponse(obj={}):
         mimetype='application/json'
     )
 
+
 def BadRequestResponse(obj={}):
     return app.response_class(
         response=json.dumps(obj),
         status=400,
+        mimetype='application/json'
+    )
+
+def InternalServerErrorResponse():
+    return app.response_class(
+        response=json.dumps({"success":False,"message":"Internal Server Error"}),
+        status=500,
         mimetype='application/json'
     )
