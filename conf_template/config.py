339f0e9ca41c8fccf52eb660f462fb41863750dc
norbeq <norbertmiazek@gmail.com>
2017-09-08 00:37:14 +0200

    [conf] Update mail and http template of config

--- conf_template/config.py
+++ conf_template/config.py
@@ -4,3 +4,12 @@ token = {
     'iss':'ankiety.mianor.pl'
 }
 db_conf = "mysql://username:password@hostname/dbname"
+mail_conf = {
+    'server':"smtp.gmail.com",
+    'port':465,
+    'use_ssl':True,
+    'username':'mail',
+    'password':'password',
+    'sender': 'sender@mail'
+}
+http_url="application url"
