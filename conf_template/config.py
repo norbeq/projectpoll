2723474881aac146e3e391884c5866b049cb9829
norbeq <norbertmiazek@gmail.com>
2017-09-14 12:17:14 +0200

    [api] Config template

--- conf_template/config.py
+++ conf_template/config.py
@@ -1,15 +1,19 @@
 token = {
-    'key':"<SECRET KEY>",
-    'expiration_seconds':'3600',
-    'iss':'iss'
+    'key': "<SECRET KEY>",
+    'expiration_seconds': 3600,
+    'iss': 'iss'
 }
 db_conf = "mysql://username:password@hostname/dbname"
 mail_conf = {
-    'server':"smtp-server",
-    'port':465,
-    'use_ssl':True,
-    'username':'mail',
-    'password':'password',
+    'server': "smtp-server",
+    'port': 465,
+    'use_ssl': True,
+    'username': 'mail',
+    'password': 'password',
     'sender': 'sender@mail'
 }
-http_url="application url"
+http_url = "application url"
+upload = {
+    'folder': "upload",
+    'allowed_extensions': set(['jpg', 'png', 'bmp'])
+}
