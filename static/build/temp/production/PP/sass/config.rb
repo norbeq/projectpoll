081c46a45c99fc5c55d122ba9c1013900f8df054
norbeq <norbertmiazek@gmail.com>
2017-09-26 01:40:34 +0200

    [app]

---
+++ static/build/temp/production/PP/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/var/www/vhosts/poll.mianor.pl/static/build/.sass-cache'
---
+++ static/build/temp/production/PP/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /var/www/vhosts/poll.mianor.pl/static/app.js
